82a367c6b3a503d3fdafa9bdee4a7e186a58516c
Ravaka Razafimanantsoa <ravaka.razafimanantsoa@porters.jp>
2023-03-14 17:49:43 +0900

    Adding CI

---
+++ .github/publish.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+export DOMAIN=ihai
+export REPOS=ai-common
+export CODEARTIFACT_REPOSITORY_URL=$(aws codeartifact get-repository-endpoint --domain $DOMAIN --repository $REPOS --format pypi --query 'repositoryEndpoint' --output text)
+export CODEARTIFACT_USER=aws
+export CODEARTIFACT_AUTH_TOKEN=$(aws codeartifact get-authorization-token --domain $DOMAIN --query 'authorizationToken' --output text)
+
+poetry build
+
+# Todo: use poetry publish when aws support.
+# (currently it return 401 error)
+pip install twine
+aws codeartifact login --tool twine --domain $DOMAIN --repository $REPOS
+twine upload --repository codeartifact dist/*
