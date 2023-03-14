8a7853cf7dec2a06422e61694dc44287e1d8265c
Ravaka Razafimanantsoa <ravaka.razafimanantsoa@porters.jp>
2023-03-14 09:47:50 +0900

    Adding jumanpp sources in sdist

--- build.py
+++ build.py
@@ -1,37 +1,65 @@
 from subprocess import Popen as shell
 from requests import get
-from setuptools.command.build_ext import build_ext
+from shutil import unpack_archive, move
+from setuptools import find_namespace_packages
 
-build_dir = "tmp"
-target_dir = "lib"
-
-
-class MakeJumanpp(build_ext):
-    def run(self) -> None:
-        shell(f"mkdir -p {build_dir}").wait()
-        shell(
-            f"cmake . -B {build_dir} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX={target_dir}",
-            shell=True,
-        ).wait()
-        shell(f"make -C {build_dir} install", shell=True).wait()
-        shell(f"rm -rf {build_dir}").wait()
-        super().run()
+target_dir: str = "lib"
+bin_dir: str = "bin"
 
 
-class GetModel(build_ext):
-    def run(self) -> None:
-        url = "https://jumanppy.s3.ap-northeast-1.amazonaws.com/jumandic.jppmdl"
-        response = get(url)
-        open(f"{target_dir}/jumandicd.jppmdl", "wb").write(response.content)
-        super().run()
+def makeJumanpp() -> None:
+    print("makeJumanpp start")
+    build_dir = "tmp"
+    shell(["mkdir", "-p", build_dir]).wait()
+    shell(
+        [
+            "cmake",
+            ".",
+            "-B",
+            build_dir,
+            "-DCMAKE_BUILD_TYPE=Release",
+            f"-DCMAKE_INSTALL_PREFIX={bin_dir}",
+        ]
+    ).wait()
+    shell(["make", "-C", build_dir, "install"]).wait()
+    shell(["rm", "-rf", build_dir]).wait()
+    print("makeJumanpp done")
 
 
-def build(setup_kwargs):
+def getModel() -> None:
+    print("getModel start")
+    build_dir = "tmp"
+    archive_path = f"{build_dir}/jumanpp.tar.xz"
+    model_path = f"{build_dir}/jumanpp-2.0.0-rc3/model/jumandic.jppmdl"
+    shell(["mkdir", "-p", build_dir]).wait()
+    url = "https://github.com/ku-nlp/jumanpp/releases/download/v2.0.0-rc3/jumanpp-2.0.0-rc3.tar.xz"
+    response = get(url)
+    open(archive_path, "wb").write(response.content)
+    unpack_archive(filename=archive_path, format="xztar", extract_dir=build_dir)
+    move(model_path, f"{bin_dir}/jumandic.jppmdl")
+    shell(["rm", "-rf", build_dir]).wait()
+    print("getModel done")
+
+
+def build(setup_kwargs: dict):
+    print("Starting custom build.py")
+    shell(["mkdir", "-p", target_dir]).wait()
+    makeJumanpp()
+    getModel()
+    packages = find_namespace_packages()
     setup_kwargs.update(
         {
-            "cmdclass": {
-                "make_jumanpp": MakeJumanpp,
-                "get_model": GetModel,
-            }
+            "packages": packages,
+            "exclude": ["dist", "build"],
+            "include_package_data": True,
+            "package_data": {
+                "jumanppy": ["*.py"],
+                "jumanpp": ["**"],
+                bin_dir: ["**"],
+            },
+            "data_files": [
+                (bin_dir, [f"{bin_dir}/libjumanppy.dylib" f"{bin_dir}/jumandic.jppmdl"])
+            ],
         }
     )
+    print(setup_kwargs)
